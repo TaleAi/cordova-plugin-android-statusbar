73f2fbf177facbdcf8e0ef010e9fbe4c11176f06
ailei <ailei@liegouchina.com>
2017-12-13 11:53:34 +0800

    添加cordova状态栏插件

--- src/android/StatusbarTransparent.java
+++ src/android/StatusbarTransparent.java
@@ -15,14 +15,21 @@ public class StatusbarTransparent extends CordovaPlugin {
 	public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
 		// grab the correct methods
 		if(action.equalsIgnoreCase("enable")) {
-			if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 				cordova.getActivity().runOnUiThread( new Runnable() {
 					public void run() {
-						cordova.getActivity().getWindow().addFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
+						setStatusBarUpperAPI21();
 					}
 				});
 				callback.success();
-			} else {
+			} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+				cordova.getActivity().runOnUiThread( new Runnable() {
+					public void run() {
+						setStatusBarUpperAPI19();
+					}
+				});
+				callback.success();
+			}else {
 				callback.error("not supported");
 			}
 			return true;
@@ -42,5 +49,21 @@ public class StatusbarTransparent extends CordovaPlugin {
 			callback.error("Unknown Action: " + action);
 			return false;
 		}
+	}
+
+	// 5.0版本以上
+	private void setStatusBarUpperAPI21() {
+		Window window = cordova.getActivity().getWindow();
+		//设置透明状态栏,这样才能让 ContentView 向上
+		window.clearFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+		//需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+		window.addFlags(LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+	}
+
+	// 4.4 - 5.0版本
+	private void setStatusBarUpperAPI19() {
+		Window window = cordova.getActivity().getWindow();
+		window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 	}
 }
